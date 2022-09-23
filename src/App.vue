ae1b1a50fb24a7428dd97c42554c020d33199003
AndrewMoonGalata <a3519110190@alumno.uttehuacan.edu.mx>
2022-09-22 22:53:13 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Nuestros platos</h1>
+      <p class="lead">
+        Inicio / Nuestros platos
+      </p>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="col-auto p-5 text-center">
+      <h1>Ensaldas y Entrantes</h1>
+    </div>
+
+    <div class="row p-4">
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+    </div>
+
+    <div class="row p-4">
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+      <div class="col-4">
+        <Card url="https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg" nombre="Tacos" descripcion="Tacos deliciosos con doble tortilla, cilantro, piña y carne al pastor" costo="5"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card" style="width: 18rem;">
+  <img v-bind:src="url" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{nombre}}</h5>
+    <p class="card-text">{{descripcion}}</p>
+
+    <p class="card-text">Precio ${{costo}}</p>
+    
+  </div>
+</div>
+</template>
+
+<script>
+    
+        export default{
+        props: {
+            url: {
+                String
+            },
+            nombre: {
+                String
+            },
+            descripcion: {
+                String
+            },
+            costo: {
+                Number
+            },
+        },
+        
+    }
+    </script>
